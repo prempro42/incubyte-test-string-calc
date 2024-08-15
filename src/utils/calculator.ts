a7d2a1e130905242f8734ee2a4fff59f153850b0
prempro42 <prempro42@gmail.com>
2024-08-15 13:45:54 +0530

    handling n number of inputs in add function and passing all test cases

--- src/utils/calculator.ts
+++ src/utils/calculator.ts
@@ -1,6 +1,12 @@
 export function add(numbers: string) {
-    // to handle the case where the input is an empty string
-    if (numbers === "") {
-      return 0;
-    }
+  // to handle the case where the input is an empty string
+  if (numbers === "") {
+    return 0;
+  }
+
+  //to split input based on ',' separator and convert to numeric strings to numbers
+  const numArray = numbers.split(",").map((num) => parseInt(num, 10));
+
+  // to add all numbers in the array & return
+  return numArray.reduce((sum, num) => sum + num, 0);
 }
