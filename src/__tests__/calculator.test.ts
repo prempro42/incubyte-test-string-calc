7ecd505fc62b155396f8ff76fc1c740edfbcb6de
prempro42 <prempro42@gmail.com>
2024-08-15 13:33:25 +0530

    test cases for single & double numeric string input

--- src/__tests__/calculator.test.ts
+++ src/__tests__/calculator.test.ts
@@ -4,4 +4,17 @@ describe("add function", () => {
   test("should return 0 for an empty string", () => {
     expect(add("")).toBe(0);
   });
+
+  test("should return the number for a single number input", () => {
+    expect(add("1")).toBe(1);
+    expect(add("7")).toBe(7);
+    expect(add("21")).toBe(21);
+  });
+
+  test("should return the sum of two numbers", () => {
+    expect(add("1,5")).toBe(6);
+    expect(add("5,5")).toBe(10);
+    expect(add("4,3")).toBe(7);
+    expect(add("7,2")).toBe(9);
+  });
 });
